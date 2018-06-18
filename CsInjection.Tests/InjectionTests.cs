326bc494eb497d7b6c4306ee5d0708b02cc8949c
Sander van den Hoed <s.vdhoed@gmail.com>
2018-06-18 22:20:18 +0200

    Start of Sandbox testing environment.

---
+++ CsInjection.Tests/InjectionTests.cs
@@ -0,0 +1,20 @@
+﻿using CsInjection.ManualMapInjection.Injection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
+
+namespace CsInjection.Tests
+{
+    [TestClass]
+    public class InjectionTests
+    {
+        [TestMethod]
+        public void ManualMapInjectionTest()
+        {
+            Process targetProcess = Process.Start("Sandbox.App.exe");
+            ManualMapInjector injector = new ManualMapInjector(targetProcess);
+            FileInfo fileInfo = new FileInfo(@"CsInjection.Cpp.Bootstrap.dll");
+            injector.Inject(fileInfo.FullName);
+        }
+    }
+}
---
+++ Sandbox.Injectable/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandbox.Injectable
+{
+    public class Injectable
+    {
+        public void Main()
+        {
+            MessageBox.Show("Hello world from injectable class");
+        }
+    }
+}
