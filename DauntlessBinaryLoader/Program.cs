1088c0b5d0b3019d4f22e37abb3cf7f7eb2a32fa
Sander van den Hoed <s.vdhoed@gmail.com>
2018-06-26 08:51:11 +0200

    dauntless stage

---
+++ DauntlessBinaryLoader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DauntlessBinaryLoader
+{
+    class Program
+    {
+        const string PackageJson = @"https://cdn.playdauntless.com/Dauntless/Production/PatcherCode/package.json";
+        public static int MyProperty { get; } = new Random().Next(0, 100);
+        public static int MyProperty1 => new Random().Next(0, 100);
+
+        static void Main(string[] args)
+        {
+            ConfigurationStorage.Get();
+        }
+       
+    }
+}
