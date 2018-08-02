f8aed8d82058de69242741e728565d52c3ce95bf
Sander van den Hoed <s.vdhoed@gmail.com>
2018-08-02 15:11:22 +0200

    Updated lolbinaryloader

--- LolBinaryLoader/Program.cs
+++ LolBinaryLoader/Program.cs
@@ -1,106 +1,56 @@
-﻿// Credits to Rockzz: https://www.unknowncheats.me/forum/2014518-post1606.html.
-
+﻿using LolBinaryLoader;
 using System;
-using System.IO;
-using System.Net;
-using System.IO.Compression;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace LoLBinaryLoader
 {
     class Program
     {
-        const string strBaseURL = "http://l3cdn.riotgames.com/releases/live/projects/lol_game_client/releases/";
-        const string strListFileName = @"releaselisting_EUW";
-
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Starting Load!");
-            WebClient wc = new WebClient();
-            try
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Task.WaitAll(new List<Task>
             {
-                wc.DownloadFile(new Uri(strBaseURL + strListFileName), strListFileName);
-            }
-            catch
+                Task.Factory.StartNew(() => new RADS(GenerateWindowsSettings()).DownloadClients()),
+                Task.Factory.StartNew(() => new RADS(GenerateMacSettings()).DownloadClients())
+            }.ToArray());
+
+            sw.Stop();
+            ReportTimeTaken(sw.ElapsedMilliseconds);
+        }
+
+        private static void ReportTimeTaken(long ms)
+        {
+            TimeSpan t = TimeSpan.FromMilliseconds(ms);
+            Console.WriteLine("Hours\tMinutes\tSeconds\tMiliseconds");
+            string answer = string.Format("{0:D2}\t{1:D2}\t{2:D2}\t{3:D3}", t.Hours, t.Minutes, t.Seconds, t.Milliseconds);
+            Console.WriteLine(answer);
+        }
+
+        private static Settings GenerateMacSettings()
+        {
+            return new Settings
             {
-                Console.WriteLine("ListNotFound:" + strBaseURL + strListFileName);
-                return;
-            }
+                ClientType = ClientType.Mac,
+                BaseUrl = "http://l3cdn.riotgames.com/releases/live/projects/lol_game_client/releases/",
+                ListFileName = "releaselisting_EUW",
+                UrlPath = "/files/League of Legends.exe.compressed"
+            };
+        }
 
-            string line;
-            StreamReader file = new StreamReader(strListFileName);
-            line = file.ReadLine();
-            //while ((line = file.ReadLine()) != null)
+        private static Settings GenerateWindowsSettings()
+        {
+            return new Settings
             {
-                // Skip any blanc lines
-                if (line == "")
-                    return;
-
-                // version URL for compressed league file
-                string strUrl = strBaseURL + line + "/files/League of Legends.exe.compressed";
-                string targetPath = $@"{line}";
-                Directory.CreateDirectory(targetPath);
-                string compressedFileName = $@"{targetPath}\League of Legends.exe.compressed";
-
-                Console.WriteLine("Downloading file:" + strUrl + "->" + compressedFileName);
-                try
-                {
-                    wc.DownloadFile(new Uri(strUrl), compressedFileName);
-                }
-                catch
-                {
-                    Console.WriteLine("No File In Release:" + strUrl);
-                    // Delete the directory if we created it and any files in the directory.
-                    if (Directory.Exists(targetPath))
-                        Directory.Delete(targetPath, true);
-                    return;
-                }
-
-                if (!File.Exists(compressedFileName))
-                {
-                    Console.WriteLine("FileNotFound:" + compressedFileName);
-                    return;
-                }
-                byte[] fileInBytes = File.ReadAllBytes(compressedFileName);
-                if (fileInBytes[0] != 0x78 || fileInBytes[1] != 0x9C)
-                {
-                    Console.WriteLine("File:" + compressedFileName + "-HeaderInvalid:" + fileInBytes[0].ToString() + "," + fileInBytes[1].ToString());
-                    return;
-                }
-                // We skip the first 2 bytes as these are only needed for the header.
-                Stream byteStreamOriginal = new MemoryStream(fileInBytes, 2, fileInBytes.Length - 2);
-                using (DeflateStream decompressionStream = new DeflateStream(byteStreamOriginal, CompressionMode.Decompress))
-                {
-                    string currentFileName = compressedFileName;
-                    string newFileName = currentFileName.Remove(currentFileName.Length - ".compressed".Length);
-                    using (FileStream decompressedFileStream = File.Create(newFileName))
-                    {
-                        decompressionStream.CopyTo(decompressedFileStream);
-                        Console.WriteLine("Decompressed: {0}", compressedFileName);
-                    }
-
-                    //Delete Compressed File!
-                    File.Delete(compressedFileName);
-                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(newFileName);
-                    string newPathName = targetPath.Replace(line, fileVersionInfo.ProductVersion);
-                    // If we have an exe file we rename the folder to that version exe, otherwise delete the folder.
-                    if (File.Exists(newFileName))
-                    {
-                        // Check if we haven't already downloaded and renamed this folder before
-                        if (!Directory.Exists(newPathName))
-                            Directory.Move(targetPath, newPathName);
-                        else
-                        {
-                            // We remove it if we already have a folder with that name as this will mean we have 
-                            //  downloaded it before and we don't want duplicates.
-                            Directory.Delete(targetPath, true);
-                            // We also want to stop the program.
-                            Environment.Exit(0);
-                        }
-                    }
-                }
-            }
-            file.Close();
+                ClientType = ClientType.Windows,
+                BaseUrl = "http://l3cdn.riotgames.com/releases/Maclive/projects/lol_game_client/releases/",
+                ListFileName = "releaselisting_EUW",
+                UrlPath = "/files/LeagueofLegends.app/Contents/MacOS/LeagueofLegends.compressed"
+            };
         }
     }
 }
---
+++ LolBinaryLoader/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolBinaryLoader
+{
+    public class Settings
+    {
+        public string BaseUrl { get; set; }
+        public string ListFileName { get; set; }
+        public string UrlPath { get; set; }
+        public ClientType ClientType { get; set; }
+    }
+
+    public enum ClientType
+    {
+        Mac,
+        Windows
+    }
+}
