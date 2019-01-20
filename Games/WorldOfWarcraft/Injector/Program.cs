1e2268b3500b0f3e64e25b04a5897a5518b4675a
Sander van den Hoed <s.vdhoed@gmail.com>
2019-01-20 20:23:56 +0100

    Moved library

---
+++ Games/WorldOfWarcraft/Injector/Program.cs
@@ -0,0 +1,23 @@
+﻿using CsInjection.Injection.Injection;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Injector
+{
+    class Program
+    {
+        private const string exe = "wow";
+        private const string dllEntryPoint = "Main";
+        private static readonly string dll = Path.Combine(Environment.CurrentDirectory, "WoWSharp.dll");
+
+        private static void Main(string[] args)
+        {
+            Process targetProcess = Process.GetProcessesByName(exe).FirstOrDefault();
+            IInjection injector = new RemoteThreadInjection(targetProcess);
+
+            injector.InjectAndExecute(dll, dllEntryPoint);
+        }
+    }
+}
---
+++ Games/WorldOfWarcraft/WoWSharp/Entrypoint.cs
@@ -0,0 +1,20 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WoWSharp
+{
+    public class Entrypoint
+    {
+        [DllExport]
+        public static void Main()
+        {
+            MessageBox.Show("Hello World");
+            Console.WriteLine("Hello world");
+        }
+    }
+}
