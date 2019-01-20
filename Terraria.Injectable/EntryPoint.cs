3f0815882604a569d595ffe536d7ae785f3cbcf4
Sander van den Hoed <s.vdhoed@gmail.com>
2019-01-20 14:32:26 +0100

    Terraria project

---
+++ Terraria.Injectable/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using CsInjection.Core.Helpers;
+using RGiesecke.DllExport;
+
+namespace Terraria.Injectable
+{
+    public class EntryPoint
+    {
+        /// <summary>
+        ///     You have to set your platform target to either x86, ia64 or x64. AnyCPU assemblies cannot export functions. 
+        /// </summary>
+        [DllExport]
+        public static void Main()
+        {
+            Log.Write("Injected");
+        }
+    }
+}
---
+++ Terraria.Injector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using CsInjection.Types;
+
+namespace Terraria.Injector
+{
+    internal class Program
+    {
+        private const string exe = "Terraria";
+        private const string dllEntryPoint = "Main";
+        private static readonly string dll = Path.Combine(Environment.CurrentDirectory, "Terraria.Injectable.dll");
+
+        private static void Main(string[] args)
+        {
+            Process targetProcess = Process.GetProcessesByName(exe).FirstOrDefault();
+            RemoteThreadInjection injector = new RemoteThreadInjection(targetProcess);
+
+            injector.Inject(dll, dllEntryPoint);
+        }
+    }
+}
