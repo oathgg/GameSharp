e937c097175866ca461dcadbfe3e9cbca0938aff
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-01 14:38:34 +0100

    Moved project

---
+++ Test/DllTest/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using CsInjection.Core.Utilities;
+using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllTest
+{
+    public class Entrypoint
+    {
+        [DllExport]
+        public static void Main()
+        {
+            ExceptionHandler.Initialize();
+            Logger.Write("Injected");
+        }
+    }
+}
--- Test/DllTest/Properties/AssemblyInfo.cs
+++ Test/DllTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InjectorTest")]
+[assembly: AssemblyTitle("DllTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InjectorTest")]
+[assembly: AssemblyProduct("DllTest")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af8fc131-6421-4fa8-8e06-4b21c214e4b8")]
+[assembly: Guid("88a0da5c-2321-48a1-811b-58ccb94411c2")]
 
 // Version information for an assembly consists of the following four values:
 //
