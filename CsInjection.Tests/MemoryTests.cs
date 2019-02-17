72d04d15c7aa1ec0847eaaa16190a676071a3349
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-17 14:34:46 +0100

    Added tests

---
+++ CsInjection.Tests/MemoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsInjection.Tests
+{
+    [TestClass]
+    public class MemoryTests
+    {
+        [TestMethod]
+        public void Write()
+        {
+
+        }
+
+        [TestMethod]
+        public void Read()
+        {
+
+        }
+
+        [TestMethod]
+        public void ReadStruct()
+        {
+
+        }
+    }
+}
