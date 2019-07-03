f650b5059d1e3762ac62d191eeb8b77bb2aac138
Sander van den Hoed <s.vdhoed@gmail.com>
2019-07-03 23:36:33 +0200

    Added a Notepad++ sample

---
+++ Sample/GameSharp.Notepadpp.Injector/Program.cs
@@ -0,0 +1,27 @@
+﻿using GameSharp.Injection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSharp.Notepadpp.Injector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The process we are injecting into.
+            Process process = Process.GetProcessesByName("notepad++").FirstOrDefault();
+
+            // A simple RemoteThreadInjector.
+            IInjection injector = new RemoteThreadInjection(process);
+
+            // Inject the DLL and executes the entrypoint.
+            string pathToDll = Path.Combine(Environment.CurrentDirectory, "GameSharp.Notepadpp.dll.dll");
+            injector.InjectAndExecute(pathToDll, "Main", attach: true);
+        }
+    }
+}
---
+++ Sample/GameSharp.Notepadpp.dll/Entrypoint.cs
@@ -0,0 +1,33 @@
+﻿using GameSharp.Utilities;
+using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSharp.Notepadpp.dll
+{
+    public class Entrypoint
+    {
+        [DllExport]
+        public static void Main()
+        {
+            Console.WriteLine("I have been injected!");
+
+            PatchRtlFindClearBits();
+        }
+
+        private static void PatchRtlFindClearBits()
+        {
+            ProcessModule module = Process.GetCurrentProcess().Modules.Cast<ProcessModule>().Where(x => x.ModuleName.ToUpper() == "NTDLL.DLL").FirstOrDefault();
+
+            Patch patch = new Patch(module.BaseAddress + 0x1010, new byte[] { 0xC3 });
+
+            patch.Enable();
+
+            Console.WriteLine("RtlFindClearBits Patched!");
+        }
+    }
+}
