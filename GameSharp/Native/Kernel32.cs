3d6c831b35c91f3ee2026f66f3d712ff600c158f
Sander van den Hoed <s.vdhoed@gmail.com>
2019-07-31 09:29:15 +0200

    Removed unused methods

--- GameSharp/Native/Kernel32.cs
+++ GameSharp/Native/Kernel32.cs
@@ -8,71 +8,52 @@ namespace GameSharp.Native
 {
     public static class Kernel32
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool IsWow64Process(IntPtr process, out bool wow64Process);
+        const string kernel32 = "kernel32.dll";
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern bool VirtualProtect(IntPtr lpAddress, int dwSize, Protection flNewProtect, out Protection lpflOldProtect);
 
-        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        internal static extern int AllocConsole();
-
-        [DllImport("kernel32.dll")]
-        internal static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        [DllImport(kernel32, CharSet = CharSet.Auto)]
         internal static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        [DllImport(kernel32, CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         internal static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
-        [DllImport("kernel32.dll")]
+        [DllImport(kernel32)]
         internal static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
-        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
-        internal static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+        [DllImport(kernel32, SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPWStr)]string lpFileName);
 
-        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        [DllImport(kernel32, SetLastError = true, CharSet = CharSet.Unicode)]
         internal static extern IntPtr LoadLibraryExW([MarshalAs(UnmanagedType.LPWStr)]string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool FreeLibrary(IntPtr hModule);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern bool CloseHandle(IntPtr hObject);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern int ResumeThread(IntPtr hThread);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern int SuspendThread(IntPtr hThread);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern IntPtr OpenThread(ThreadAccessFlags dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport(kernel32, SetLastError = true)]
+        internal static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern bool GetThreadContext(IntPtr hThread, ref Context lpContext);
+        [DllImport(kernel32, SetLastError = true)]
+        internal static extern bool GetThreadContext(IntPtr hThread, ref Structs.Context lpContext);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern bool SetThreadContext(IntPtr hThread, ref Context lpContext);
-
-        /// <summary>
-        ///     Retrieves information about a range of pages within the virtual address space of a specified process.
-        /// </summary>
-        /// <param name="processHandle"></param>
-        /// <param name="baseAddress"></param>
-        /// <param name="buffer"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(kernel32, SetLastError = true)]
         internal static extern bool VirtualQueryEx(IntPtr processHandle, IntPtr baseAddress, out MemoryBasicInformation buffer, int length);
     }
 }
