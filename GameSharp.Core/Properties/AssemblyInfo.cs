c9b82f86473ac643d368881967645fa1bf6d4133
Sander van den Hoed <s.vdhoed@gmail.com>
2020-02-06 10:47:51 +0100

    Updated assembly info

--- GameSharp.Core/Properties/AssemblyInfo.cs
+++ GameSharp.Core/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameSharp.Shared")]
+[assembly: AssemblyTitle("GameSharp.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameSharp.Shared")]
+[assembly: AssemblyProduct("GameSharp.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
