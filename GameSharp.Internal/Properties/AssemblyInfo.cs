a5cc42dd3eb2d81cb1e800e38a074aec34b8088e
Sander van den Hoed <s.vdhoed@gmail.com>
2020-02-06 10:49:10 +0100

    Updated assembly

--- GameSharp.Internal/Properties/AssemblyInfo.cs
+++ GameSharp.Internal/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameSharp.Internal")]
+[assembly: AssemblyTitle("GameSharp.Internal.dll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameSharp.Internal")]
+[assembly: AssemblyProduct("GameSharp.Internal.dll")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
